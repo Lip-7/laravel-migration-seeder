723c2a6266d5645095ad9f2d55af620c8100df8b
Lip-7 <zhighofficial@gmail.com>
2023-05-24 23:57:07 +0200

    geeeeeeeeeeeeeeeeeet

--- app/Http/Controllers/TodayController.php
+++ app/Http/Controllers/TodayController.php
@@ -8,7 +8,7 @@ class TodayController extends Controller
 {
     public function index(){
         $today = date('Y-m-d') ;
-        $trains = Train::where('date', $today);
+        $trains = Train::where('date', $today)->get();
         return view('today', compact('trains', 'today'));
     }
 }
