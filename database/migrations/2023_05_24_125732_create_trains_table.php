48dba56e664dc0e9be501aa73f02adb4902ce7e0
Lip-7 <zhighofficial@gmail.com>
2023-05-24 15:06:16 +0200

    init, scaffholding and creating table

---
+++ database/migrations/2023_05_24_125732_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('company', 150);
+            $table->string('leaving_station');
+            $table->string('arriving_station');
+            $table->time('leaving_time', $precision = 0);
+            $table->time('arriving_time', $precision = 0);
+            $table->string('train_code',32);
+            $table->smallInteger('carriages')->unsigned();
+            $table->boolean('in_time')->default(0);
+            $table->boolean('deleted')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
