9e196a20637a75d5d5df03b48e94bce663dac08a
Lip-7 <zhighofficial@gmail.com>
2023-05-30 09:35:10 +0200

    created TrainController and fixed the routes

---
+++ app/Http/Controllers/TrainController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Train;
+use Illuminate\Http\Request;
+
+class TrainController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $trains = Train::all();
+        return view('home', compact('trains'));
+    }
+    public function today()
+    {
+        $today = date('Y-m-d') ;
+        $trains = Train::where('date', $today)->get();
+        return view('today', compact('trains', 'today'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TodayController;
+use App\Http\Controllers\TrainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/today', [TodayController::class, 'index'])->name('today');
+//Route::get('/', [HomeController::class, 'index'])->name('home');
+//Route::get('/today', [TodayController::class, 'index'])->name('today');
+Route::get('/', [TrainController::class, 'index'])->name('home');
+Route::get('/today', [TrainController::class, 'today'])->name('today');
